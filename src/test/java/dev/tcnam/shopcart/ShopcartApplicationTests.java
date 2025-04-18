98610ebc7a9ece2226f0ef74ebf109442f5fb27f
tcnam <tcnam2000@gmail.com>
2025-04-19 05:28:18 +0700

    first commit

---
+++ src/test/java/dev/tcnam/shopcart/ShopcartApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tcnam.shopcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopcartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
